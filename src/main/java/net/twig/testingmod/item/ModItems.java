6551da861186fc1b36dee8dd841f76bcfb00fef3
Gamemastertwig <brandonlocker@hotmail.com>
2025-04-29 16:40:49 -0400

    Refactoring

--- src/main/java/net/twig/testingmod/item/ModItems.java
+++ src/main/java/net/twig/testingmod/item/ModItems.java
@@ -5,21 +5,36 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.twig.testingmod.TestingMod;
 
-public class ModItems {
-    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+import java.util.function.Function;
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(TestingMod.MOD_ID, name), item);
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet", Item::new, new Item.Settings());
+//    private static Item registerItem(String name, Item item) {
+//        return Registry.register(Registries.ITEM, Identifier.of(TestingMod.MOD_ID, name), item);
+//    }
+
+    public static Item registerItem(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TestingMod.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
     }
 
     public static void registerModItems() {
         TestingMod.LOGGER.info("Registering Mod Items for " + TestingMod.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(PINK_GARNET);
-        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(ModItems.PINK_GARNET));
     }
 }
