f12f695ee50a9d9d4548a2473047675347e9a5ca
Gamemastertwig <brandonlocker@hotmail.com>
2025-04-29 10:32:58 -0400

    Initial Commit

---
+++ src/main/java/net/twig/testingmod/TestingMod.java
@@ -0,0 +1,16 @@
+package net.twig.testingmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestingMod implements ModInitializer {
+	public static final String MOD_ID = "testingmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/twig/testingmod/TestingModClient.java
@@ -0,0 +1,10 @@
+package net.twig.testingmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestingModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
